4b25a6acd761d235e47f5a55ea3b8493044638af
Nicolás Contreras <78387846+nicontrerasc8@users.noreply.github.com>
2022-08-08 17:41:03 -0500

    Create CV.js

---
+++ Components/CV.js
@@ -0,0 +1,24 @@
+import { motion, useScroll } from "framer-motion"
+
+const Projects = () => {
+  return <div className='page-section'>
+     <h2>
+          Información personal:
+     </h2>
+     <section className='grid'>
+          <motion.article
+            initial={{ opacity: 0, right: "100px" }}
+            whileInView={{ opacity: 1, right: "0px" }} key={idx}
+          >
+                    <h5 className='colored-text'>Curriculum vitae</h5>
+                         <a href={data.link} target="_blank" rel="noreferrer">
+                              <button className='button color'>
+                                   Descargar CV
+                              </button>
+                         </a>
+          </motion.article>
+     </section>
+  </div>
+}
+
+export default Projects
