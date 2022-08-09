a616c1941febcf3539dda398cfd1f731bffdce93
nicontrerasc8 <nicontrerasc8@gmail.com>
2022-08-08 23:04:50 -0500

    si

--- Components/Projects.js
+++ Components/Projects.js
@@ -62,7 +62,13 @@ const Data = [
 ]
 
 const Projects = () => {
-  return <div className='page-section'>
+  return <>
+  <div className="custom-shape-divider-top-1659718043">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+    </svg>
+</div>
+     <div className='page-section'>
      <h2>
           Mis proyectos:
      </h2>
@@ -90,6 +96,7 @@ const Projects = () => {
           }
      </section>
   </div>
+  </>
 }
 
 export default Projects
--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,6 @@ const Home = () => {
       </section>
       <img className="main-img" src={"https://firebasestorage.googleapis.com/v0/b/prochristo-b4aea.appspot.com/o/Dise%C3%B1o%20sin%20t%C3%ADtulo%20(9).png?alt=media&token=d71d1009-3019-46f2-8038-3093853fe802"}/>
     </div>
-    <div className="custom-shape-divider-top-1659718043">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-    </svg>
-</div>
     <Projects/>
     <div className="custom-shape-divider-top-1659718043 up">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -36,11 +31,6 @@ const Home = () => {
     </svg>
 </div>
 <Studies/>
-          <div className="custom-shape-divider-top-1659718043">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-    </svg>
-</div>
   <CV/>
   </>
 }
